a23cfe80eb4a90f8ce4382184fbc8ffd142944e2
barzansaeedpour <barzansaeedpour@gmail.com>
2024-12-24 12:35:29 +0330

    working on selecting different sections of dxf file

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ import geopandas as gpd
 
 
 ##################################### dwg to dxf
-INPUT_FOLDER = "./input/"
-OUTPUT_FOLDER = "./output"
+# INPUT_FOLDER = "./input/"
+# OUTPUT_FOLDER = "./output"
 
 
-TEIGHA_PATH = "C:/Program Files/ODA/ODAFileConverter 25.11.0/ODAFileConverter.exe"
-OUTVER = "ACAD2018"
-OUTFORMAT = "DXF"
-RECURSIVE = "0"
-AUDIT = "1"
-INPUTFILTER = "*.DWG"
+# TEIGHA_PATH = "C:/Program Files/ODA/ODAFileConverter 25.11.0/ODAFileConverter.exe"
+# OUTVER = "ACAD2018"
+# OUTFORMAT = "DXF"
+# RECURSIVE = "0"
+# AUDIT = "1"
+# INPUTFILTER = "*.DWG"
 
-# Command to run
-cmd = [TEIGHA_PATH, INPUT_FOLDER, OUTPUT_FOLDER, OUTVER, OUTFORMAT, RECURSIVE, AUDIT, INPUTFILTER]
+# # Command to run
+# cmd = [TEIGHA_PATH, INPUT_FOLDER, OUTPUT_FOLDER, OUTVER, OUTFORMAT, RECURSIVE, AUDIT, INPUTFILTER]
 
-# Run
-subprocess.run(cmd, shell=True)
+# # Run
+# subprocess.run(cmd, shell=True)
 
 
 ##################################### dxf to shape
