b5f7b35adbcfc9d6fb1d85709ac6fc675e223b9f
barzansaeedpour <barzansaeedpour@gmail.com>
2024-12-24 13:19:26 +0330

    first successfull cropping

--- main.py
+++ main.py
@@ -23,17 +23,52 @@ import geopandas as gpd
 
 ##################################### dxf to shape
 
+# data = gpd.read_file("./output/Khoroseh Var-Plan & Profile.dxf")
+
+# data['geom_type']= data.geometry.type
+
+# # split lines and points
+# data_lines = data.loc[data.geom_type == 'LineString']
+# data_points = data.loc[data.geom_type == 'Point']
+
+# # export the data to shapefiles
+# data_lines.to_file("./output/lines.shp")
+# data_points.to_file("./output/points.shp")
+
+
+import geopandas as gpd
+from shapely.geometry import box
+
+# Read the DXF file into a GeoDataFrame
 data = gpd.read_file("./output/Khoroseh Var-Plan & Profile.dxf")
 
-data['geom_type']= data.geometry.type
+# Add a new column for geometry type
+data['geom_type'] = data.geometry.type
 
-# split lines and points
-data_lines = data.loc[data.geom_type == 'LineString']
-data_points = data.loc[data.geom_type == 'Point']
+# Calculate the bounding box of all features in the GeoDataFrame
+bounding_box = data.total_bounds  # Returns (minx, miny, maxx, maxy)
+min_x, min_y, max_x, max_y = bounding_box
+print(f"Bounding Box: {bounding_box}")
 
-# export the data to shapefiles
-data_lines.to_file("./output/lines.shp")
-data_points.to_file("./output/points.shp")
+# Create a bounding box polygon
+# bounding_box_polygon = box(min_x, min_y, max_x, max_y)
+bounding_box_polygon = box(0, min_y, 21000, max_y)
+
+# Select features within the bounding box
+cropped_data = data[data.geometry.intersects(bounding_box_polygon)]
+
+# Split cropped data into lines and points
+data_lines = cropped_data[cropped_data['geom_type'] == 'LineString']
+data_points = cropped_data[cropped_data['geom_type'] == 'Point']
+
+# Export the cropped LineString geometries to a shapefile
+data_lines.to_file("./output/cropped_lines.shp")
+
+# Export the cropped Point geometries to a shapefile
+data_points.to_file("./output/cropped_points.shp")
+
+print("Cropped data conversion completed!")
+
 
 
 
